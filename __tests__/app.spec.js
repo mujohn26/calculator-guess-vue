0357de356b3f577db14c3a638b4eae5df5d0ccff
mujohn26 <mujohn26@gmail.com>
2021-10-13 15:53:15 +0200

    add tests

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -6,3 +6,36 @@ describe("App",() => {
     expect(typeof App.data).toBe('function')
   })
 })
+
+describe("Mounted app", () => {
+
+  const wrapper = mount(App)
+  test("is vue instance", () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+  it("renders correct html text", () => {
+    expect(wrapper.html()).toContain("let us test your arthimetic")
+    expect(wrapper.html()).toContain("What is the sum of two numbers")
+  })
+  it("has a button", () => {
+    expect(wrapper.contains('button')).toBe(true)
+  })
+  it("renders correctly set data", async() => {
+    wrapper.setData({ x1: 5, x2: 10 })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.text()).toContain("15")
+  })
+  it('button click without correct sum', () => {
+    expect(wrapper.vm.message).toBe("")
+    const button = wrapper.find('button')
+    button.trigger('click')
+    expect(wrapper.vm.message).toBe('TRY AGAIN')
+  })
+
+  it('button click with correct sum', () => {
+    wrapper.setData({ guess: "15" })
+    const button = wrapper.find('button')
+    button.trigger('click')
+    expect(wrapper.vm.message).toBe('SUCCESS !')
+  })
+})
